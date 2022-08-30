c76df89e84a0aa259f6c3c906309f1a099e59ef1
OruamC <oru_catani@hotmail.com>
2022-08-30 16:55:51 -0300

    Adding float point

--- app/src/main/java/com/oruam/mycalculator/MainActivity.kt
+++ app/src/main/java/com/oruam/mycalculator/MainActivity.kt
@@ -1,15 +1,16 @@
 package com.oruam.mycalculator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     private var tvInput: TextView? = null
+    var lastNumeric : Boolean = false
+    var lastDot : Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,9 +21,19 @@ class MainActivity : AppCompatActivity() {
 
     fun onDigit(view: View) {
         tvInput?.append((view as Button).text)
+        lastNumeric = true
+        lastDot = false
     }
 
     fun onClear(view: View) {
         tvInput?.text = ""
+    }
+
+    fun onDecimalPoint(view: View) {
+        if(lastNumeric && !lastDot) {
+            tvInput?.append(".")
+            lastNumeric = false
+            lastDot = true
+        }
     }
 }
